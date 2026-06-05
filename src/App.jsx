a48d105d75e7c0a3ef84036176656d6cc036de1b
nguyennn0411 <phucnguyenxt2004@gmail.com>
2026-06-05 12:51:18 +0700

    chat bot

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ import CheckoutPage from './pages/shop/CheckoutPage';
 import ShopOrdersPage from './pages/shop/ShopOrdersPage';
 import AdminProducts from './pages/admin/AdminProducts';
 import AdminShopOrders from './pages/admin/AdminShopOrders';
-
+import AiChatWidget from './components/AiChatWidget';
 
 export default function App() {
   return (
@@ -111,6 +111,7 @@ export default function App() {
         <Route path='/services' element={<Navigate to='/search' replace />} />
         <Route path='*' element={<Navigate to='/' replace />} />
       </Routes>
+        <AiChatWidget />
     </BrowserRouter>
   );
 }
